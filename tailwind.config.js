c2a5a9469070e89b8da4828f714b3732d9702bff
Nouman Usman <2022CS49@student.uet.edu.pk>
2024-12-23 11:33:21 +0500

    Add tailwindcss-animate plugin to Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from 'tailwindcss-animate';
 export default {
+	plugins: [tailwindcssAnimate],
 	darkMode: ["class"],
 	content: [
 		"./index.html",
@@ -30,5 +32,5 @@ export default {
 			},
 		}
 	},
-	plugins: [require("tailwindcss-animate")],
+	// plugins: [require("tailwindcss-animate")],
 }
