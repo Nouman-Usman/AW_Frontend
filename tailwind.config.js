650eb8b75a30b50a87a4192b8ee4250caff6543c
Nouman Usman <2022CS49@student.uet.edu.pk>
2025-01-03 14:29:06 +0500

    ADDED tailwind fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import tailwindcssAnimate from "tailwindcss-animate";
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	darkMode: ["class"],
@@ -30,5 +32,5 @@ export default {
 			},
 		}
 	},
-	plugins: [require("tailwindcss-animate")],
+	plugins: [tailwindcssAnimate],
 }
